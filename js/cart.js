8a70308008c77d6e074993979d2b340ca75c832d
Aleksey <lukerlexa@gmail.com>
2017-09-23 14:31:51 +0300

    Сделал рабочим range

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,5 @@
 var cart={};//Корзина
+var rezu=0;
 var p=0;
 var dataTab=0;
 var dataTabKr=0;
@@ -11,27 +12,22 @@ $(function () {
     $('.visa').click(function () {
         dataTab=0;
         fTabs();
-        fTabsTexterea();
     });
     $('.mastercard').click(function () {
         dataTab=1;
         fTabs();
-        fTabsTexterea();
     });
     $('.yamoney').click(function () {
         dataTab=2;
         fTabs();
-        fTabsTexterea();
     });
     $('.paypal').click(function () {
         dataTab=3;
         fTabs();
-        fTabsTexterea();
     });
     $('.webmoney').click(function () {
         dataTab=4;
         fTabs();
-        fTabsTexterea();
     });
 
 
@@ -68,16 +64,25 @@ $(function () {
         $('.numberOpl').css('display','block');
         $('.oplata textarea').css('display','none');
         $('.back').css('display','none');
-        $('.kreditForm').css('display','none');
+        $('.kartForm').css('display','none');
     });
+
 
     $("select").change(function(){
         if($(this).val() == '')
             return false;
 
-        if($(this).val()=="Наличными") {
+        if($(this).val()=="Наличными"){
+            $('.kartForm').css('display','none');
+            $(".kreditForm").css('display', 'none');
+            $(".kredit").css('display', 'none');
+            $(".oplata").css('display', 'none');
+        }
+
+        if($(this).val()=="Карточкой") {
             $(".kredit").css('display', 'none');
             $(".oplata").css('display', 'block');
+            $('.kartForm').css('display','none');
             $('.kreditForm').css('display','none');
             $('.numberOpl').css('display','block');
             $('.back').css('display','none');
@@ -86,18 +91,27 @@ $(function () {
         if($(this).val()=="Кредит") {
             $(".oplata").css('display', 'none');
             $(".kredit").css('display', 'block');
+            $('.kartForm').css('display','none');
             $('.kreditForm').css('display','none');
             $('.numberOplKredit').css({
                 'width':'',
                 'height':''
-            })
+            });
         }
     });
 
 
-    $('.kredit img, .oplata img').click(function () {
 
+    $('.oplata img').click(function () {
+        $('.kartForm').css('display','block');
+    });
+
+    $('.kredit img').click(function () {
         $('.kreditForm').css('display','block');
+    });
+
+    $(".kartForm .right").click(function () {
+        $('.kartForm').css('display','none');
     });
 
     $(".kreditForm .right").click(function () {
@@ -159,7 +173,179 @@ $(function () {
     });
 
 
+    var RangeSlider = function(containerID) {
+        var self = this,
+            $RangeSlider = $('#'+containerID),
+            $SliderThumnb = $RangeSlider.find('.RangeSlider_Thumb'),
+            $SliderTrack = $RangeSlider.find('.RangeSlider_Track'),
+            $SliderTrackFill = $RangeSlider.find('.RangeSlider_TrackFill'),
+            $ClickArea = $RangeSlider.find('.RangeSlider_ClickArea'),
+            $SliderPoints = $RangeSlider.find('.RangeSlider_Point');
+
+        this.value = 0;
+
+        /* helper to find slider value based on filled track width */
+        var findValueFromTrackFill = function(trackFillWidth) {
+            var totalWidth = $SliderTrack.width(),
+                onePercentWidth = totalWidth / 100,
+                value = Math.round((trackFillWidth / onePercentWidth) / 10);
+            return value;
+        };
+
+        /* change highlighted number based on new value */
+        var updateSelectedValue = function(newValue) {
+            $SliderPoints.removeClass('RangeSlider_PointActive');
+            $SliderPoints.eq( newValue ).addClass('RangeSlider_PointActive');
+        };
+
+        /* highlight track based on new value (and move thumb) */
+        var updateHighlightedTrack = function(newPosition) {
+            newPosition = newPosition + '0%';
+            $SliderTrackFill.css('width', newPosition);
+        };
+
+        /* set up drag funcationality for thumbnail */
+        var setupDrag = function($element, initialXPosition) {
+            $SliderTrackFill.addClass('RangeSlider_TrackFill-stopAnimation');
+            var trackWidth = $SliderTrackFill.width();
+
+            var newValue = findValueFromTrackFill( trackWidth );
+            updateSelectedValue(newValue);
+
+            $element.on('mousemove', function(e){
+                var newPosition = trackWidth + e.clientX - initialXPosition;
+                self.imitateNewValue( newPosition );
+
+                newValue = findValueFromTrackFill( $SliderTrackFill.width() );
+                updateSelectedValue(newValue);
+            });
+        };
+        /* remove drag functionality for thumbnail */
+        var finishDrag = function($element) {
+            $SliderTrackFill.removeClass('RangeSlider_TrackFill-stopAnimation');
+            $element.off('mousemove');
+            var newValue = findValueFromTrackFill( $SliderTrackFill.width() );
+            self.updateSliderValue( newValue );
+        };
+
+        /* method to update all things required when slider value updates */
+        this.updateSliderValue = function(newValue) {
+            updateSelectedValue( newValue );
+            updateHighlightedTrack( newValue );
+            self.value = newValue;
+            console.log('this.value = ', self.value);
+
+            if(self.value==0){
+                $('.monye').html('');
+            }
+            if(self.value==1){
+                $('.monye').html(((rezu/6)+(rezu*0.03)).toFixed(2));
+            }
+            if(self.value==2){
+                $('.monye').html(((rezu/12)+(rezu*0.03)).toFixed(2));
+            }
+            if(self.value==3){
+                $('.monye').html(((rezu/18)+(rezu*0.03)).toFixed(2));
+            }
+            if(self.value==4){
+                $('.monye').html(((rezu/24)+(rezu*0.03)).toFixed(2));
+            }
+            if(self.value==5){
+                $('.monye').html(((rezu/30)+(rezu*0.03)).toFixed(2));
+            }
+            if(self.value==6){
+                $('.monye').html(((rezu/36)+(rezu*0.03)).toFixed(2));
+            }
+            if(self.value==7){
+                $('.monye').html(((rezu/42)+(rezu*0.03)).toFixed(2));
+            }
+            if(self.value==8){
+                $('.monye').html(((rezu/48)+(rezu*0.03)).toFixed(2));
+            }
+            if(self.value==9){
+                $('.monye').html(((rezu/54)+(rezu*0.03)).toFixed(2));
+            }
+            if(self.value==10){
+                $('.monye').html(((rezu/60)+(rezu*0.03)).toFixed(2));
+            }
+        };
+
+        /* method to imitate new value without animation */
+        this.imitateNewValue = function(XPosition) {
+            $SliderTrackFill.addClass('RangeSlider_TrackFill-stopAnimation');
+            if ( XPosition > $SliderTrack.width() ) {
+                XPosition = $SliderTrack.width();
+            }
+            $SliderTrackFill.css('width', XPosition + 'px');
+        };
+
+        /*
+         * bind events when instance created
+         */
+        $ClickArea.on('mousedown', function(e){
+            /* if a number or thumbnail has been clicked, use their event instead */
+            var $target = $(e.target);
+            if ( $target.hasClass('RangeSlider_Thumb') ) {
+                return false;
+            }
+            /* now we can continue based on where the clickable area was clicked */
+            self.imitateNewValue( e.offsetX );
+            setupDrag( $(this), e.clientX );
+        });
+
+        $ClickArea.on('mouseup', function(e){
+            finishDrag( $(this) );
+        });
+
+        // update value when markers are clicked
+        $SliderPoints.on('mousedown', function(e){
+            e.stopPropagation();
+            var XPos = $(this).position().left + ($(this).width()/2);
+            self.imitateNewValue( XPos );
+            setupDrag( $ClickArea, e.clientX );
+        });
+
+        // when thumbnail is clicked down, init the drag stuff
+        $SliderThumnb.on('mousedown', function(e){
+            e.stopPropagation();
+            setupDrag( $(this), e.clientX );
+        });
+
+        // when the thumbnail is released, remove the drag stuff
+        $SliderThumnb.on('mouseup', function(e){
+            finishDrag( $(this) );
+        });
+    };
+
+    var rangeSlider = new RangeSlider('RangeSlider');
+    var rangeSlider2 = new RangeSlider('RangeSlider2');
+
+
+
 });
+
+function addYear(flag) {
+    var s=document.forms[0].year;
+    var val, o;
+    if(flag){
+        val=s.options[s.length-1].value*1+1;
+        o=new Option(val,val,false,true);
+        try {
+            s.add(o,null);
+        }catch (e){
+            s.add(o);
+        }
+    }
+    else {
+        val=s.options[0].value*1-1;
+        o=new Option(val,val,false,true);
+        try {
+            s.add(o,s.options[0]);
+        }catch (e){
+            s.add(o,0);
+        }
+    }
+}
 
 function fTabs() {
     var number=$('.numberOpl');
@@ -190,15 +376,7 @@ function fTabs_2() {
     }
 }
 
-function fTabsTexterea() {
-    var texterea=$('.oplata textarea');
-    for(var i=0;i<texterea.length;i++){
-        if(dataTab==i)
-            texterea[i].style.display='block';
-        else
-            texterea[i].style.display='none';
-    }
-}
+
 
 $.getJSON('goods.json', function (data) {
     var goods=data;//все товары в массиве
@@ -239,7 +417,7 @@ $.getJSON('goods.json', function (data) {
                 p = goods[key].cost * cart[key];
                 rezalt.push(p);
             }
-            var rezu=0;
+
             for(var i=0;i<rezalt.length;i++)
                 rezu+=rezalt[i];
 
@@ -275,53 +453,6 @@ $.getJSON('goods.json', function (data) {
         loadGoodsCart();
         showMiniCart();
     }
-
-    // function plusGoods(e) {
-    //     //добавляем по 1 товару
-    //
-    //     var counter;
-    //     var element = e.target;
-    //     var articul = $(this).attr('data-art');
-    //
-    //     if((cart[articul]!=null)) {
-    //         cart[articul]++;
-    //     }
-    //     else{
-    //         cart[articul]=1;
-    //     }
-    //
-    //
-    //
-    //     $.getJSON('goods.json', function (data) {
-    //         for (var key in data)
-    //             counter = data[articul].count;
-    //
-    //         if (counter < cart[articul]) {
-    //             alert("Вы выбрали больше дисков, чем на складе");
-    //             cart[articul] = counter;
-    //             saveCartToLS();
-    //             loadGoods();
-    //             showMiniCart();
-    //         }
-    //     });
-    //
-    //
-    //     saveCartToLS();
-    //     loadGoods();
-    //     showMiniCart();
-    //
-    //     function inputType() {
-    //
-    //         for(var key in cart) {
-    //             if (articul == key) {
-    //                 $(element).siblings(1).val(cart[articul]);
-    //             }
-    //
-    //         }
-    //     }
-    //     inputType();
-    //
-    // }
 
     function minusGoodsCart() {
         //убираем по 1 товару
